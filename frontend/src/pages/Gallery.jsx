88fb18cc88873b5fe7c80b014901b2436d82558e
Jformani <jformani04@outlook.com>
2025-09-04 21:28:04 -0400

    Fetched movies from API and display single page result to Gallery.jsx

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,10 +1,43 @@
+import React, { useState, useEffect } from "react";
 
+function Gallery() {
+  const [movieList, setMovieList] = useState([]);
 
-export default function Gallery() {
+  async function getMovie() {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc&api_key=${
+          import.meta.env.VITE_TMDB_KEY
+        }`
+      );
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+      const data = await response.json();
+      setMovieList((prev) => [...prev, ...data.results]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-    return(
-        <>
-    
-        </>
-    )
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  function handleLoad() {
+    setMovieList((prev) => [...prev, ...data.results]);
+  }
+
+  return (
+    <div>
+      {movieList.map((movie) => (
+        <img
+          key={movie.id}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+      ))}
+    </div>
+  );
 }
+
+export default Gallery;
